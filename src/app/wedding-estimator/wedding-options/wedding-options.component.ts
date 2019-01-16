8f126a2b1faa383cd71f43e6e4b719897fa53247
mmoretto <marco_moretto24@hotmail.com>
2019-01-16 14:01:52 -0500

    added the form

--- src/app/wedding-estimator/wedding-options/wedding-options.component.ts
+++ src/app/wedding-estimator/wedding-options/wedding-options.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-wedding-options',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wedding-options.component.css']
 })
 export class WeddingOptionsComponent implements OnInit {
-
+@ViewChild('f') form: NgForm;
   constructor() { }
+
+  result: number;
+  flowers: number;
 
   ngOnInit() {
   }
+  onSubmit(form:NgForm){
 
+    if(form.value.optradio === 'true'){
+     this.flowers = (form.value.guests/10 * 200)
+     this.result = (form.value.guests * form.value.hall) + +form.value.dj + this.flowers;
+    }else{
+      this.result = (form.value.guests * form.value.hall) + +form.value.dj;
+    }
+    console.log(form.value);
+    console.log(this.result);
+    
+  }
 }
