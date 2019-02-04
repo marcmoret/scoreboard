6ff7c0452e6719a0aa8409aad383b8e4449e5338
mmoretto <marco_moretto24@hotmail.com>
2019-02-04 17:05:03 -0500

    big progress

--- src/app/ideas/ideas.component.ts
+++ src/app/ideas/ideas.component.ts
@@ -30,6 +30,10 @@ export class IdeasComponent implements OnInit {
   postDoc2: AngularFirestoreDocument;
   
 
+  postCol3: AngularFirestoreCollection<Idea>;
+  posts3:any;
+  postDoc3: AngularFirestoreDocument<any>;
+
   name: string;
   idea: string;
 
@@ -47,6 +51,8 @@ export class IdeasComponent implements OnInit {
   ngOnInit() {
     this.postCol = this.afs.collection('profiles');
    // this.posts = this.postCol.valueChanges();
+
+
   
     this.posts = this.postCol.snapshotChanges()
     .map(actions =>{
@@ -72,6 +78,7 @@ export class IdeasComponent implements OnInit {
     this.name = this.name.charAt(0).toUpperCase() + this.name.substr(1);
     //capitlizes first name no matter how they enter it//
 
+
     this.postCol2 = this.afs.collection('profiles').doc(this.name).collection('results');
    
      this.posts2 = this.postCol2.snapshotChanges()
@@ -83,7 +90,7 @@ export class IdeasComponent implements OnInit {
        })
      });
 
-   // this.testArray.push(this.testArray + this.idea);
+  //  // this.testArray.push(this.testArray + this.idea);
     this.afs.collection('profiles').doc(this.name).collection('results').add({'ideas':this.idea, 'date': this.today});
     this.afs.collection('profiles').doc(this.name).set({'date': this.today});
 
@@ -102,6 +109,26 @@ export class IdeasComponent implements OnInit {
       }));
     };
     startAsync(text => console.log());
+}
+
+onGet(e:any){
+
+  console.log(e);
+  this.name = e;
+  this.postCol2 = this.afs.collection('profiles').doc(e).collection('results');
+
+  this.posts2 = this.postCol2.snapshotChanges()
+     .map(actions =>{
+       return actions.map(a => {
+         const data = a.payload.doc.data();
+         const id = a.payload.doc.id;
+         return { id, data};
+       })
+     });
+
+    
+     
+  
 }
 
 }
