17367ac6b23fd31f95cb730fb549ed3e917139ad
mmoretto <marco_moretto24@hotmail.com>
2019-02-14 08:37:10 -0500

    testing and sidebar prototype

--- src/app/testing/lvl2/lvl2.component.ts
+++ src/app/testing/lvl2/lvl2.component.ts
@@ -8,32 +8,61 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
   styleUrls: ['./lvl2.component.css'],
   animations: [
     trigger('navigation', [
-        state('true' , style({ left:'-20%'})), 
+        state('true' , style({ left:'-15%'})), 
         state('false', style({ left:'0%'})),
         transition('0 => 1', animate('.2s')),
         transition('1 => 0', animate('.3s'))
 
     ]),
-    trigger('showOverlay', [
-        state('true' , style({ opacity: 1,display:"block" })), 
-        state('false', style({ opacity: 0,display:"none" })),
-        transition('0 => 1', animate('.2s')),
-        transition('1 => 0', animate('.5s'))
-    ])
+    trigger('openClose', [
+      
+      state('open', style({
+        
+        opacity: 1,
+        backgroundColor: '#f9f9ee'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        display:'none',
+        backgroundColor: '#f9f9ee'
+      })),
+      transition('open => closed', [
+        animate('.5s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
   ]
 })
 export class Lvl2Component implements OnInit {
 
-  events: string[] = [];
-  opened: boolean;
+  slideInOut:boolean = false;
 
   navigation:boolean = true;
   showOverlay:boolean = false;
+  state:boolean =true;
+  
+  
   navigationDrawer(){
-      this.navigation = !this.navigation;
-      this.showOverlay = !this.showOverlay;
+
+    const wait = (ms) => new Promise(res => setTimeout(res, ms));
+        const startAsync = async callback => {
+          await wait(100);
+          this.navigation = !this.navigation;
+        };
+        startAsync(text => console.log());
+  }
+  navigationDrawer2(){
+    this.navigation = !this.navigation;
   }
 
+  isOpen = false;
+ 
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
   
   constructor() { }
 
--- src/app/testing/lvl4/lvl4.component.ts
+++ src/app/testing/lvl4/lvl4.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-lvl4',
+  animations: [
+    trigger('openClose', [
+      
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
   templateUrl: './lvl4.component.html',
   styleUrls: ['./lvl4.component.css']
 })
@@ -10,6 +32,12 @@ export class Lvl4Component implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  isOpen = true;
+ 
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
