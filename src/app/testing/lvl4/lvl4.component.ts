5dbaa06df0714f4e7e358cb9c9257e6749fcb446
mmoretto <marco_moretto24@hotmail.com>
2019-02-14 17:22:54 -0500

    updates

--- src/app/testing/lvl4/lvl4.component.ts
+++ src/app/testing/lvl4/lvl4.component.ts
@@ -1,49 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { CdkAccordionItem } from '@angular/cdk/accordion';
-import { matExpansionAnimations,MatExpansionPanelState } from '@angular/material';
 
 @Component({
   selector: 'app-lvl4',
-  animations: [ 
-    matExpansionAnimations.bodyExpansion,
-    trigger('openClose', [
-      
-      state('open', style({
-        height: '225px',
-      })),
-      state('closed', style({
-        height: '0px',
-        overflowY:'hidden'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s ease-out')
-      ]),
-    ]),
-  ],
   templateUrl: './lvl4.component.html',
   styleUrls: ['./lvl4.component.css']
 })
-export class Lvl4Component extends CdkAccordionItem implements OnInit {
-
-  getExpandedState(): MatExpansionPanelState {
-    return this.expanded ? 'expanded' : 'collapsed';
-  }
+export class Lvl4Component implements OnInit {
 
   ngOnInit() {
-  }
-
-  isOpen = true;
- 
-  toggle1() {
-    this.isOpen = !this.isOpen;
-  }
-
-  toggleAccordion() : void {
-    this.toggle();
   }
 
 }
