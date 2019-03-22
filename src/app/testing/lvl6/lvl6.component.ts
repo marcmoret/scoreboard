f8f8205910825edaf40963085657c25edb5849e1
mmoretto <marco_moretto24@hotmail.com>
2019-03-22 17:10:29 -0400

    things

--- src/app/testing/lvl6/lvl6.component.ts
+++ src/app/testing/lvl6/lvl6.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, Directive } from '@angular/core';
+import { Component, OnInit, Directive, Output } from '@angular/core';
 import * as $ from 'jquery'; 
 
 @Component({
@@ -8,6 +8,8 @@ import * as $ from 'jquery';
 })
 @Directive({selector: 'button[counting]'})
 export class Lvl6Component implements OnInit {
+  checke: any;
+  bigTest;
 
   constructor() { }
 
@@ -15,10 +17,48 @@ export class Lvl6Component implements OnInit {
 
   }
 
+  test;
+
   numberOfClicks = 0;
- 
-  @HostListener('mouseover') onHover() {
-    window.alert("hover");
+
+  calc(){
+    console.log(this.test)
+    this.test = this.wipeout(this.test)
+    
   }
+
+  onClick(){
+    this.test = this.test + .67;
+    console.log(this.checke);
+
+  }
+  wipeout(value: string){
+    let rawValue = value ? value : '0.00';
+
+    // Remove non-digit characters
+    rawValue = value.replace(/([^\d])/g, '');
+
+    // Add decimal point
+    // NOTE: For some reason the decimal/comma point (from the parameter value)
+    // is placed at the third position from last instead of second from last as it should be
+    const decimalPoint = '.';
+    const decimalPosition = rawValue.length - 2;
+
+    if (!rawValue || /^(0[\.\,])?0+$/.test(rawValue)) { // Matches: 0... - 0,00... - 0.00...
+      rawValue = '0.00';
+    } else {
+      rawValue = [
+        rawValue.slice(0, decimalPosition),
+        decimalPoint,
+        rawValue.length === 1 ? '0' : '',
+        rawValue.slice(decimalPosition)
+      ].join('');
+    }
+    console.log(parseFloat(rawValue).toFixed(2))
+    return parseFloat(rawValue).toFixed(2);
+
+  // return thing = thing + ' mhm ';
+  }
+
  }
 
